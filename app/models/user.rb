c5a1ba4a5ee97490e57b96fae98f85b474db5bfb
Samuel Jefferson <sam_jefferson@hotmail.co.uk>
2016-03-16 16:31:21 +0000

    first steps on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+validates :username, presence: true, length: { within: 6..20}, 
+							uniqueness: { case_sensitive: false }
+has_secure_password
+PASSWORD_REGEX = /\A(?=.*\d)(?=.*[a-zA-Z])[A-Za-z\d]+\z/
+
+validates :password, presence: true, length: { within: 6..16 },
+							 format: { with: PASSWORD_REGEX }
+def User.digest(string)
+	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+end                                              
 end
